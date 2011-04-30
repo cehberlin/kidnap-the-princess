3dd4384decab7c2b68fcd388c67090ab35337e7a
c.hrabia <c.hrabia@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-04-30 07:21:40 +0000

    missing file

---
+++ trunk/KidnapThePrincess/KidnapThePrincess/KidnapThePrincess/Templar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KidnapThePrincess
+{
+    class Templar:Enemy
+    {
+        public Templar(Texture2D tex, List<Hero> heroes)
+            : base(tex,heroes)
+        {
+            Speed = 0.8f;
+        }
+    }
+}
