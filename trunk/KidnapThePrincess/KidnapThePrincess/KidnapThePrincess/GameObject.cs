5a2752f15726d01d32e4081909db6f9f54f359c9
pascal.schwenke@googlemail.com <pascal.schwenke@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-05-01 06:04:58 +0000

    Improved collision handling. When the player collides with GameObjects he "slides" around them. I also forgot to actually add the new classes.

---
+++ trunk/KidnapThePrincess/KidnapThePrincess/KidnapThePrincess/GameObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace KidnapThePrincess
+{
+    class GameObject
+    {
+        Texture2D sprite;
+
+        private Vector2 pos;
+
+        public Vector2 Position
+        {
+            get { return pos; }
+            set { pos = value; }
+        }
+
+        private Rectangle area;
+
+        public Rectangle Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+        public Rectangle CollisionArea
+        {
+            get { return new Rectangle(
+                (int)(area.X + area.Width * 0.125),
+                (int)(area.Y + area.Height * 0.25),
+                (int)(area.Width * 0.75),
+                (int)(area.Height * 0.75));
+            }
+            set { area = value; }
+        }
+
+        private int hitpoints;
+
+        public int Hitpoints
+        {
+            get { return hitpoints; }
+            set { hitpoints = value; }
+        }
+
+        public GameObject(Texture2D tex)
+        {
+            sprite = tex;
+        }
+        public virtual void Draw(SpriteBatch sb) 
+        {
+            sb.Draw(sprite, Area, Color.White);
+        }
+
+        public virtual void Update(GameTime time) { }
+    }
+}
---
+++ trunk/KidnapThePrincess/KidnapThePrincess/KidnapThePrincess/Hut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace KidnapThePrincess
+{
+    class Hut : GameObject
+    {
+        public Hut(Texture2D tex, Vector2 pos)
+            : base(tex)
+        {
+            Position = pos;
+            Area = new Rectangle((int)pos.X, (int)pos.Y, tex.Width, tex.Height);
+            Hitpoints = 20;
+        }
+    }
+}
