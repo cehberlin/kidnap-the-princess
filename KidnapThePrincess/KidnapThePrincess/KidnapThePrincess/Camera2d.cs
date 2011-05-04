46e2ce9ca29c302b5a1d391470cde2802ee828de
amaurialb@googlemail.com <amaurialb@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-05-04 18:12:01 +0000

    Camera adjusted. Please check the solution.

--- KidnapThePrincess/KidnapThePrincess/KidnapThePrincess/Camera2d.cs
+++ KidnapThePrincess/KidnapThePrincess/KidnapThePrincess/Camera2d.cs
@@ -17,6 +17,15 @@ namespace KidnapThePrincess
         public Matrix _transform; // Matrix Transform
         public Vector2 _pos; // Camera Position
         protected float _rotation; // Camera Rotation
+       
+        private Vector2 speed;//Camera speed
+        private Vector2 targetPosition;//Target position
+
+        public Vector2 TargetPosition
+        {
+            get { return targetPosition; }
+            set { targetPosition = value; }
+        }
         private int width;
 
         public int Width
@@ -32,6 +41,24 @@ namespace KidnapThePrincess
             get { return height; }
             set { height = value; }
         }
+
+        private int maxVelocity;
+
+        public int MaxVelocity
+        {
+            get { return maxVelocity; }
+            set { maxVelocity = value; }
+        }
+
+        private int acceleration;
+
+        public int Acceleration
+        {
+            get { return acceleration; }
+            set { acceleration = value; }
+        }
+        
+        
 
         /// <summary>
         /// The area the camera covers.
@@ -76,6 +103,50 @@ namespace KidnapThePrincess
         public void Move(Vector2 amount)
         {
             _pos += amount;
+        }
+        
+        //set/get Speed
+        public Vector2 Speed
+        {
+            get {return speed;}
+            set {
+                if (speed.Length() > maxVelocity)
+                {
+                    speed.Normalize();
+                    speed *= maxVelocity;
+                }
+                else speed = value;
+            }
+        }
+
+        public void IncrementSpeed(Vector2 _acceleration)
+        {
+            speed += _acceleration;
+            if (speed.Length() > maxVelocity)
+            {
+                speed.Normalize();
+                speed *= maxVelocity;
+            }
+        }
+
+        //Follow
+
+        public void FollowTarget(float time)
+        {
+            Vector2 direction;
+            //check if the target is reached
+            if (Math.Abs(_pos.X - targetPosition.X) > 2 || Math.Abs(_pos.Y - targetPosition.Y) > 2)
+            {
+                //find direction
+                if (_pos.X > targetPosition.X) direction.X = -1;
+                else direction.X = 1;
+
+                if (_pos.Y > targetPosition.Y) direction.Y = -1;
+                else direction.Y = 1;
+
+                _pos += speed * time *direction;
+
+            }
         }
         // Get set position
         public Vector2 Pos
