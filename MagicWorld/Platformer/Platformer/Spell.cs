74efc860f5208b33cb8c07658c91479fa2460b0a
amaurialb@googlemail.com <amaurialb@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-05-14 07:52:50 +0000

    Classes that were missing were added

---
+++ MagicWorld/Platformer/Platformer/Spell.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer
+{
+    /// <summary>
+    /// Base class that determines a spell
+    /// </summary>
+    abstract class Spell
+    {
+        #region properties
+        /// <summary>
+        /// Origin from the object in the screen
+        /// </summary>
+        public Vector2 Origin;
+
+        public enum State { CREATING, WORKING };
+        public State spellState;
+        /// <summary>
+        /// Actual size of the object
+        /// the idea is that the object on the moment of creation grows
+        /// </summary>
+        public Rectangle Size;
+        public Rectangle MaxSize;
+        public Rectangle MinSize;
+        /// <summary>
+        /// Actual Position
+        /// </summary>
+        public Vector2 Position;
+
+        /// <summary>
+        /// Force of the spell.
+        /// It may work as factor that scale distance,velocity or time
+        /// </summary>
+        private int force;
+        public int Force
+        {
+            set { force = value; }
+            get { return force; }
+        }
+
+        protected Level level;
+
+        protected SpriteEffects flip = SpriteEffects.None;
+
+        /// <summary>
+        /// velocity of the movement of the spell
+        /// </summary>
+        protected Vector2 velocity;
+        public Vector2 Velocity
+        {
+            set { velocity = value; }
+            get { return velocity; }
+        }
+
+        protected float direction;
+
+        public float Direction
+        {
+            set { direction = value; }
+            get { return direction; }
+        }
+
+        /// <summary>
+        /// Texture of object
+        /// </summary>
+        private Texture2D spellTexture;
+        public Texture2D SpellTexture
+        {
+            set {spellTexture = value;}
+        }
+
+        protected float time;
+        
+        private float survivalTime;
+        public float SurvivalTime
+        {
+            set {survivalTime=value;}
+            get {return survivalTime;}
+        }
+
+        // Animations
+        protected Animation runAnimation;
+        protected Animation idleAnimation;
+        protected AnimationPlayer sprite;
+
+        #endregion
+        
+        #region methods
+
+        public Spell(string spriteSet, Vector2 _origin,  Level level)
+        {
+            //spellTexture = _texture;
+            Origin = _origin;
+            Velocity = Vector2.Zero;
+            Force = 0;
+            Position = _origin;
+            time = 0;
+            this.level = level;
+            spellState = State.CREATING;
+            LoadContent(spriteSet);
+        }
+        
+
+
+        /// <summary>
+        /// Loads a particular enemy sprite sheet and sounds.
+        /// </summary>
+        public virtual void LoadContent(string spriteSet)
+        {
+            
+        }
+
+        public Rectangle BoundingRectangle
+        {
+            get
+            {
+                int left = (int)Math.Round(Position.X - sprite.Origin.X) + Size.X;
+                int top = (int)Math.Round(Position.Y - sprite.Origin.Y) + Size.Y;
+
+                return new Rectangle(left, top, Size.Width, Size.Height);
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) 
+        {
+            // Flip the sprite to face the way we are moving.
+            if (direction > 0)
+                flip = SpriteEffects.FlipHorizontally;
+            else if (direction < 0)
+                flip = SpriteEffects.None;
+
+            // Draw that sprite.
+            sprite.Draw(gameTime, spriteBatch, Position, flip);
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+            
+        }
+        public void Grow()
+        {
+        }
+        public void Shrink()
+        {
+        }
+        #endregion
+    }
+}
---
+++ MagicWorld/Platformer/Platformer/WarmSpell.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace Platformer
+{
+    class WarmSpell:Spell 
+    {
+        private const float MoveSpeed = 64.0f;
+
+        public WarmSpell(string spriteSet,Vector2 _origin,Level level):base(spriteSet,  _origin, level)
+        {            
+            Force = 1;
+            time = 2;
+            LoadContent(spriteSet);
+            sprite.PlayAnimation(idleAnimation);
+        }
+
+        public override void LoadContent(string spriteSet)
+        {
+            // Load animations.
+            spriteSet = "Sprites/" + spriteSet + "/";
+            runAnimation = new Animation(level.Content.Load<Texture2D>(spriteSet + "Run"), 0.1f, true);
+            idleAnimation = new Animation(level.Content.Load<Texture2D>(spriteSet + "Idle"), 0.15f, true);
+            sprite.PlayAnimation(idleAnimation);
+
+            // Calculate bounds within texture size.
+            int width = (int)(idleAnimation.FrameWidth * 1);
+            int left = (idleAnimation.FrameWidth - width) / 2;
+            int height = (int)(idleAnimation.FrameWidth * 0.7);
+            int top = idleAnimation.FrameHeight - height;
+            Size = new Rectangle(left, top, width, height);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            //the fire lives a little bit until reach an obstacle or die due to time
+
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Calculate tile position based on the side we are walking towards.
+            float posX = Position.X + Size.Width / 2 * (int)direction;
+            int tileX = (int)Math.Floor(posX / Tile.Width) - (int)direction;
+            int tileY = (int)Math.Floor(Position.Y / Tile.Height);
+
+            
+            // Move in the current direction.
+            velocity = new Vector2((int)direction * MoveSpeed * elapsed, 0.0f);
+            Position = Position + velocity;
+
+                
+            
+        }
+    }
+}
