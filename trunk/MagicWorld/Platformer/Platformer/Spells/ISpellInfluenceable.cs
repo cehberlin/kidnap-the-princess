629ec6fdf802252e4214910fde3d5f76211e964d
c.hrabia <c.hrabia@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-05-14 15:03:50 +0000

    Interface for alle gameobjects which could be influenced by spell action. Do something like if(spell.GetType()==typeof(WarmSpell)){...} to react on a specific spell

---
+++ trunk/MagicWorld/Platformer/Platformer/Spells/ISpellInfluenceable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+    interface ISpellInfluenceable
+    {
+        /// <summary>
+        /// The class needs to implement the action which is forced by a spell on its own
+        /// </summary>
+        /// <param name="spell"></param>
+        public virtual void SpellInfluenceAction(Spell spell);
+    }
+}
