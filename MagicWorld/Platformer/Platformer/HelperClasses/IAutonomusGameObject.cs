c592e0517f3a715c1f1b6475cdd62ce9dc8a8a4f
c.hrabia <c.hrabia@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-05-14 16:49:38 +0000

    mssing file last commit

---
+++ MagicWorld/Platformer/Platformer/HelperClasses/IAutonomusGameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Platformer
+{
+    /// <summary>
+    /// Interface which encapsulates default Load Update Draw behavior of a autonomus object (all objects which handle this on its own)
+    /// exception is the player!the player is another type of special gameobjects
+    /// </summary>
+    interface IAutonomusGameObject
+    {
+        void LoadContent(string spriteSet);
+        void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+        void Update(GameTime gameTime);
+    }
+}
