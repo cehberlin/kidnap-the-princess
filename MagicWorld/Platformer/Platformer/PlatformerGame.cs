357769e77ab5a34d5712bbb0d03fd5e92d9bd5df
pascal.schwenke@googlemail.com <pascal.schwenke@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-05-18 10:44:55 +0000

    Graphical changes. Added dummy file for Icehit.wma

--- MagicWorld/Platformer/Platformer/PlatformerGame.cs
+++ MagicWorld/Platformer/Platformer/PlatformerGame.cs
@@ -210,6 +210,8 @@ namespace Platformer
         {
             graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            //for (int i = 0; i <= level.layers.Length; ++i)
+            //    spriteBatch.Draw(level.layers[i], Vector2.Zero, Color.White);
 
             //spriteBatch.Begin();
             spriteBatch.Begin(SpriteSortMode.Immediate,
