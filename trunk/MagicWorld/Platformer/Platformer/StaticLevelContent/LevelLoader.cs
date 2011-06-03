8e60e33b23361b955b16bce1ef87891334a59ae8
c.hrabia <c.hrabia@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-06-03 14:30:27 +0000

    preparation for new level loading structure

---
+++ trunk/MagicWorld/Platformer/Platformer/StaticLevelContent/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MagicWorld;
+using MagicWorld.DynamicLevelContent;
+using Microsoft.Xna.Framework;
+
+namespace MagicWorld.StaticLevelContent
+{
+    /// <summary>
+    /// Interface for new Level Loaders
+    /// </summary>
+    interface LevelLoader
+    {
+        List<Enemy> getEnemies();
+        
+        List<BasicGameElement> getGeneralObjects();
+        
+        List<Icecicle> getIcecicles();
+
+        Vector2 getPlayerStartPosition();
+
+        BasicGameElement getLevelExit();
+
+        double getMaxLevelTime();
+    }
+}
---
+++ trunk/MagicWorld/Platformer/Platformer/StaticLevelContent/StaticLevelLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicWorld.StaticLevelContent
+{
+    /// <summary>
+    /// Static fixed implementation of a test level
+    /// </summary>
+    class StaticLevelLoader:LevelLoader
+    {
+        #region LevelLoader Member
+
+        public List<Enemy> getEnemies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DynamicLevelContent.BasicGameElement> getGeneralObjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Icecicle> getIcecicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Microsoft.Xna.Framework.Vector2 getPlayerStartPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DynamicLevelContent.BasicGameElement getLevelExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double getMaxLevelTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
