3a161fff6472e6b28e7487a30ea7f0f16c1e3ebe
c.hrabia <c.hrabia@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-06-03 14:30:27 +0000

    preparation for new level loading structure

---
+++ MagicWorld/Platformer/Platformer/StaticLevelContent/LevelLoaderFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicWorld.StaticLevelContent
+{
+    class LevelLoaderFactory
+    {
+        /// <summary>
+        /// Factory which gives you a new LevelLoader
+        /// </summary>
+        /// <param name="nr"></param>
+        /// <returns></returns>
+        public static LevelLoader getLevel(int nr){
+
+            switch (nr) { 
+                case 1:
+                    throw new NotImplementedException();                   
+                default:
+                    return new StaticLevelLoader();
+            }
+    }
+    }
+}
