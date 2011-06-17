cc76b591fa41b60d79e371906dd89c614b990807
John.Lanvin.Calo@gmail.com <John.Lanvin.Calo@gmail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-06-17 17:31:08 +0000

    IEnemyService Class

---
+++ trunk/MagicWorld/Platformer/Platformer/Services/IEnemyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using MagicWorld.HelperClasses;
+
+namespace MagicWorld.Services
+{
+    interface IEnemyService : IServiceProvider
+    {
+        Vector2 Position { get; }
+        Bounds Bounds { get; }
+        Boolean IsBurning { get; set; }
+        Boolean IsFroozen { get; set; }
+        Boolean IsElectrified { get; set; }
+
+    }
+}
