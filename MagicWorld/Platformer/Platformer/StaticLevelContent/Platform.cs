3d746f8c5ce47c29ea87710b1ebdbd18bbfd5369
drm.one.85@googlemail.com <drm.one.85@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-06-17 15:11:00 +0000

    changed Platform bounds (new class!) changed physics changed player bounds

---
+++ MagicWorld/Platformer/Platformer/StaticLevelContent/Platform.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MagicWorld.DynamicLevelContent;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MagicWorld.HelperClasses;
+using MagicWorld.Constants;
+
+namespace MagicWorld.StaticLevelContent
+{
+    /// <summary>
+    /// platform the player can stand on
+    /// </summary>
+    class Platform : BlockElement
+    {
+
+        private Rectangle drawRec;
+
+        public Platform(String texture, CollisionType collision,Level level,Vector2 position)
+            : base(texture, collision, level, position)
+        {
+            
+            int left = (int)Math.Round(position.X);
+            int top = (int)Math.Round(position.Y);
+
+            int yOffset = 10;
+            this.bounds = new Bounds(left + yOffset/2, top, Width - yOffset, 20);
+            drawRec = new Rectangle(left, top, Width, Height);
+        }
+
+
+        private Bounds bounds;
+        public override HelperClasses.Bounds Bounds
+        {
+            get
+            {
+                return this.bounds;
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(base.Texture, drawRec, Color.White);
+            if (DebugValues.DEBUG)
+            {
+                spriteBatch.Draw(debugTexture, Bounds.getRectangle(), debugColor);
+            }
+        }
+    }
+}
