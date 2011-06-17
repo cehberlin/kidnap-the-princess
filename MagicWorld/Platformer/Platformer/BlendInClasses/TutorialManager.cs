af5987b5ab9c4feed1d6cfa6b07ef889f39da894
pascal.schwenke@googlemail.com <pascal.schwenke@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-06-17 08:27:18 +0000

    Quit and Exit can be used again. TutorialInstruction.cs and TutorialManager.cs can later be used to display messages for the player. It needs to be extended as it is just in a very basic state right now.

---
+++ MagicWorld/Platformer/Platformer/BlendInClasses/TutorialManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MagicWorld.BlendInClasses
+{
+    class TutorialManager : DrawableGameComponent
+    {
+        //TODO: add some basic scripting/placement trough level editor/add texture/add custom font
+        List<TutorialInstruction> instructions;
+        SpriteFont font;
+        SpriteBatch spriteBatch;
+
+        public void AddInstruction(String text)
+        {
+            TutorialInstruction t = new TutorialInstruction(text);
+            t.Manager = this;
+            instructions.Add(t);
+        }
+
+        public TutorialManager(Game game)
+            : base(game)
+        {
+            instructions = new List<TutorialInstruction>();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            font = Game.Content.Load<SpriteFont>("Instructions/InstructionFont");
+            spriteBatch = new SpriteBatch(Game.GraphicsDevice);
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                if (instructions[i].IsActive)
+                {
+                    if (instructions[i].DisplayTime <= TimeSpan.Zero)
+                        instructions.Remove(instructions[i]);
+                    else
+                        instructions[i].DisplayTime = instructions[i].DisplayTime.Subtract(gameTime.ElapsedGameTime);
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                if (instructions[i].IsActive)
+                {
+                    spriteBatch.DrawString(font, instructions[i].Text, instructions[i].Position, instructions[i].Color);
+                }
+            }
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
