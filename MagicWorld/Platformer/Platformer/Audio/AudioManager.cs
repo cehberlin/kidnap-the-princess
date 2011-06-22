bcd94a352f7c326eb404c4b2586f4eee6daddb39
pascal.schwenke@googlemail.com <pascal.schwenke@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-06-22 21:16:17 +0000

    Forgot to add a file. Audiomanager will be finished later.

---
+++ MagicWorld/Platformer/Platformer/Audio/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using System.IO;
+
+namespace MagicWorld.Audio
+{
+    class AudioManager : GameComponent
+    {
+        String path = "Sounds/";
+        List<SoundEffect> sounds;
+
+        public AudioManager(Game game)
+            : base(game)
+        {
+            sounds = new List<SoundEffect>();
+        }
+
+        public override void Initialize()
+        {
+            //sounds.Add(Game.Content.Load<SoundEffect>(path + "CreateSpell"));
+            SoundEffect s = Game.Content.Load<SoundEffect>(path + "CreateSpell");
+            sounds.Add(s);
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "ExitReached"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "GemCollected"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "Icehit"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "Monsterkilled"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "PlayerFall"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "PlayerJump"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "PlayerKilled"));
+            sounds.Add(Game.Content.Load<SoundEffect>(path + "Powerup"));
+        }
+    }
+}
