11591d8bbcd25d5aabfb7fa3f586eef7e88e26fb
c.hrabia <c.hrabia@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-07-01 08:47:35 +0000

    improved level 5 fixed some bugs with portals still problem the drawing position of a portal is not like expected?? check level 5. Could someone help?

---
+++ trunk/MagicWorld/Platformer/Platformer/DynamicLevelContent/Enemies/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicWorld.DynamicLevelContent.Enemies
+{
+    /// <summary>
+    /// dangerous flying objects are described as own enemy
+    /// </summary>
+    //class Bullet:Enemy
+    //{
+    //}
+}
