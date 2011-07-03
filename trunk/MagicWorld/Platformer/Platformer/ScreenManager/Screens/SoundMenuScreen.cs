385603f7e1de738516dc8f4cb37c23568ae944a1
amaurialb@googlemail.com <amaurialb@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-07-03 20:23:08 +0000

    Sound Screen working. Right now just for backgroung Music. Display screen showing status.

---
+++ trunk/MagicWorld/Platformer/Platformer/ScreenManager/Screens/SoundMenuScreen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+
+namespace MagicWorld
+{
+    class SoundMenuScreen : MenuScreen
+    {
+        #region Initialization
+
+        bool playingBackMusic=false;
+        MenuEntry mnuPlayBackGroundMusic;
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SoundMenuScreen()
+            : base("Options")
+        {
+            // Create our menu entries.
+
+            mnuPlayBackGroundMusic = new MenuEntry("Background Music <Off>");            
+
+            MenuEntry back = new MenuEntry("Back");
+
+            // Hook up menu event handlers.
+            mnuPlayBackGroundMusic.Selected += PlayBackgroundMusic;          
+            back.Selected += OnCancel;
+            
+            // Add entries to the menu.
+            MenuEntries.Add(mnuPlayBackGroundMusic);
+            
+            MenuEntries.Add(back);
+        }
+
+        #endregion
+
+        #region Handle Input
+
+        /// <summary>
+        /// Event handler for when the Ungulate menu entry is selected.
+        /// </summary>
+        void PlayBackgroundMusic(object sender, PlayerIndexEventArgs e)
+        {
+            
+            //Known issue that you get exceptions if you use Media PLayer while connected to your PC
+            //See http://social.msdn.microsoft.com/Forums/en/windowsphone7series/thread/c8a243d2-d360-46b1-96bd-62b1ef268c66
+            //Which means its impossible to test this from VS.
+            //So we have to catch the exception and throw it away
+            if (playingBackMusic)
+            {
+                mnuPlayBackGroundMusic.Text = "Background Music <Off>";
+                MediaPlayer.Stop();
+            }
+            else
+            {
+                mnuPlayBackGroundMusic.Text = "Background Music <On>";
+                try
+                {
+                    MediaPlayer.IsMuted = false;
+                    MediaPlayer.IsRepeating = true;
+                    MediaPlayer.Play(ScreenManager.ContentManager.Load<Song>("Sounds/Backgroundmusic"));
+                }
+                catch { }
+            }
+
+            playingBackMusic=!playingBackMusic;
+
+        }
+       
+
+        #endregion
+    }
+}
