515b54e5d783c2f8089cf743f8340592c0af0983
John.Lanvin.Calo@gmail.com <John.Lanvin.Calo@gmail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-07-09 15:47:24 +0000

    AudioManager implemented (just basics not yet finished)

---
+++ trunk/MagicWorld/Platformer/Platformer/Audio/IAudioService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicWorld.Audio
+{
+    public interface IAudioService : IServiceProvider
+    {
+        void playSound(SoundType soundType);
+    }
+}
