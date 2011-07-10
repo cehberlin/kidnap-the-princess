797f24342e9e2a6f6b7b3cd8748e4574bf59cf33
drm.one.85@googlemail.com <drm.one.85@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-07-10 23:46:46 +0000

    balanced moveable platform speed

---
+++ MagicWorld/Platformer/Platformer/Constants/LevelElements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagicWorld.Constants
+{
+    public class LevelElements
+    {
+        private LevelElements() { }
+
+        public const float MoveablePlatformSpeed = 85f;
+        public const float MoveableSpellPlatformSpeed = 150f;
+    }
+}
