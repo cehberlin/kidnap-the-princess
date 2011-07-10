456cc43664114967635086408f9cd4d9dca42ec6
drm.one.85@googlemail.com <drm.one.85@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-07-10 23:56:51 +0000

    balanced bat boundings

--- MagicWorld/Platformer/Platformer/DynamicLevelContent/Enemies/Bat.cs
+++ MagicWorld/Platformer/Platformer/DynamicLevelContent/Enemies/Bat.cs
@@ -65,11 +65,11 @@ namespace MagicWorld
                 }
                 else
                 {
-                    float width = (sprite.Animation.FrameWidth * 0.5f);
-                    float height = (sprite.Animation.FrameHeight * 0.5f);
+                    float width = (sprite.Animation.FrameWidth * 0.55f);
+                    float height = (sprite.Animation.FrameHeight * 0.4f);
                     float left = (float)Math.Round(Position.X - width / 2);
                     float top = (float)Math.Round(Position.Y - height / 2);
-                    return new Bounds(left, top, width, height);
+                    return new Bounds(left, top + 5, width, height);
                 }
             }
         }
