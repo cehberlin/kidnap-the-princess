5b415fd8a17efba03503a3af053163fc20333fe6
drm.one.85@googlemail.com <drm.one.85@googlemail.com@45a3b83f-b2cf-83ff-18ff-75d5cc7cc64b>
2011-07-14 13:43:40 +0000

    polished credits

--- MagicWorld/Platformer/Platformer/ScreenManager/Screens/CreditsScreen.cs
+++ MagicWorld/Platformer/Platformer/ScreenManager/Screens/CreditsScreen.cs
@@ -43,22 +43,24 @@ namespace MagicWorld
         {
             const String tab = "    ";
 
-            message = "MAGIC WORLD" + "\n" + "\n" + "\n";
+            message = "Game Over" + "\n" + "\n" + "\n" + "\n" + "\n";
+
+            message += "MAGIC WORLD" + "\n" + "\n";
 
             message += "made at TU-Berlin" + "\n";
             message += "for Game Programming" + "\n";
             message += "summer 2011" + "\n";
-            message += "by Team \"INNOVATIVE ENTERTAINMENT\"" + "\n" + "\n" + "\n";
+            message += "by Team \"INNOVATIVE ENTERTAINMENT\"" + "\n" + "\n";
+
+            //message += "GAMEPLAY PROGRAMMER:" + "\n";
+            message += tab + "Amauri Albuquerque " + "\n";
+            message += tab + "John Calo" + "\n";
 
             //message += "GRAPHICS PROGRAMMER:" + "\n";
             message += tab + "Christopher Hrabia" + "\n";
 
             //message += "CONTENT CREATOR:" + "\n";
             message += tab + "Pascal Schwenke" + "\n";
-
-            //message += "GAMEPLAY PROGRAMMER:" + "\n";
-            message += tab + "John Calo" + "\n";
-            message += tab + "Amauri Albuquerque " + "\n";
 
             //message += "TEAM LEAD:" + "\n";
             message += tab + "Marian Volk" + "\n";
